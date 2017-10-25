362a7f043b4e23f927c5040fef39252e86a1cb29
Islam Elnabarawy <islam.ossama@gmail.com>
2017-10-24 22:35:19 -0500

    Raise the version number by one revision and list dependencies in setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ __author__ = 'Islam Elnabarawy'
 
 setup(
     name='sc2gym',
-    version='0.0.2',
-    install_requires=['gym', 'pysc2'],
+    version='0.0.3',
+    install_requires=['gym', 'pysc2', 'absl', 'numpy'],
     url='https://github.com/islamelnabarawy/sc2gym'
 )
