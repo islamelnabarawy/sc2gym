a4efab71b8d2c772772fa3e92e22dcd0689922eb
Islam Elnabarawy <islam.ossama@gmail.com>
2017-10-24 23:16:02 -0500

    Fix requires name for absl to absl-py

--- setup.py
+++ setup.py
@@ -5,6 +5,6 @@ __author__ = 'Islam Elnabarawy'
 setup(
     name='sc2gym',
     version='0.0.3',
-    install_requires=['gym', 'pysc2', 'absl', 'numpy'],
+    install_requires=['gym', 'pysc2', 'absl-py', 'numpy'],
     url='https://github.com/islamelnabarawy/sc2gym'
 )
