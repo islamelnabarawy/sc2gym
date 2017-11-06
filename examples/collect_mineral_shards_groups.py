4758e51e1dc41957ffca1f2228ffd8784c2bb31c
Islam Elnabarawy <islam.ossama@gmail.com>
2017-11-05 20:02:17 -0600

    Add example for CollectMineralShards-v2

---
+++ examples/collect_mineral_shards_groups.py
@@ -0,0 +1,43 @@
+import sys
+
+import numpy as np
+import gym
+from absl import flags
+
+# noinspection PyUnresolvedReferences
+import sc2gym.envs
+
+__author__ = 'Islam Elnabarawy'
+
+FLAGS = flags.FLAGS
+
+_PLAYER_FRIENDLY = 1
+_PLAYER_NEUTRAL = 3  # beacon/minerals
+_NO_OP = 0
+
+_NUM_EPISODES = 10
+
+
+def main():
+    FLAGS(sys.argv)
+
+    env = gym.make("SC2CollectMineralShards-v2")
+    env.settings['visualize'] = False
+
+    episode_reward = np.zeros((_NUM_EPISODES, ))
+    for ix in range(_NUM_EPISODES):
+        obs = env.reset()
+
+        done = False
+        while not done:
+            action = env.action_space.sample()
+            obs, reward, done, _ = env.step(action)
+            episode_reward[ix] += reward
+
+        print('Episode {} reward: {}'.format(env.episode, episode_reward[ix]))
+
+    print('Average reward for {} episodes: {}'.format(_NUM_EPISODES, np.mean(episode_reward)))
+
+
+if __name__ == "__main__":
+    main()
