f7ee81df3f9c88dbe830a0e4e7d4ea3d6cc446dc
Islam Elnabarawy <islam.ossama@gmail.com>
2017-11-06 23:49:07 -0600

    Guard public fields of base environment with read-only properties

--- sc2gym/envs/sc2_game.py
+++ sc2gym/envs/sc2_game.py
@@ -21,16 +21,16 @@ class SC2GameEnv(gym.Env):
         self._kwargs = kwargs
         self._env = None
 
-        self.episode = 0
-        self.num_step = 0
-        self.episode_reward = 0
-        self.total_reward = 0
+        self._episode = 0
+        self._num_step = 0
+        self._episode_reward = 0
+        self._total_reward = 0
 
     def _step(self, action):
         return self._safe_step(action)
 
     def _safe_step(self, action):
-        self.num_step += 1
+        self._num_step += 1
         if action[0] not in self.available_actions:
             logger.warning("Attempted unavailable action: %s", action)
             action = [_NO_OP]
@@ -44,22 +44,22 @@ class SC2GameEnv(gym.Env):
             return None, 0, True, {}
         self.available_actions = obs.observation['available_actions']
         reward = obs.reward
-        self.episode_reward += reward
-        self.total_reward += reward
+        self._episode_reward += reward
+        self._total_reward += reward
         return obs, reward, obs.step_type == StepType.LAST, {}
 
     def _reset(self):
         if self._env is None:
             self._init_env()
-        if self.episode > 0:
+        if self._episode > 0:
             logger.info("Episode %d ended with reward %d after %d steps.",
-                        self.episode, self.episode_reward, self.num_step)
+                        self._episode, self._episode_reward, self._num_step)
             logger.info("Got %d total reward so far, with an average reward of %g per episode",
-                        self.total_reward, float(self.total_reward) / self.episode)
-        self.episode += 1
-        self.num_step = 0
-        self.episode_reward = 0
-        logger.info("Episode %d starting...", self.episode)
+                        self._total_reward, float(self._total_reward) / self._episode)
+        self._episode += 1
+        self._num_step = 0
+        self._episode_reward = 0
+        logger.info("Episode %d starting...", self._episode)
         obs = self._env.reset()[0]
         self.available_actions = obs.observation['available_actions']
         return obs
@@ -71,11 +71,11 @@ class SC2GameEnv(gym.Env):
         self._env = sc2_env.SC2Env(**self._kwargs)
 
     def _close(self):
-        if self.episode > 0:
+        if self._episode > 0:
             logger.info("Episode %d ended with reward %d after %d steps.",
-                        self.episode, self.episode_reward, self.num_step)
+                        self._episode, self._episode_reward, self._num_step)
             logger.info("Got %d total reward, with an average reward of %g per episode",
-                        self.total_reward, float(self.total_reward) / self.episode)
+                        self._total_reward, float(self._total_reward) / self._episode)
         self._env.close()
         super()._close()
 
@@ -94,3 +94,19 @@ class SC2GameEnv(gym.Env):
         if self._env is None:
             self._init_env()
         return self._env.observation_spec()
+
+    @property
+    def episode(self):
+        return self._episode
+
+    @property
+    def num_step(self):
+        return self._num_step
+
+    @property
+    def episode_reward(self):
+        return self._episode_reward
+
+    @property
+    def total_reward(self):
+        return self._total_reward
